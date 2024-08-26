0390f8168a15a9cdfe3334b2d9e4846bc1704ae1
Kevin Lin <linkevin281@gmail.com>
2024-08-26 12:50:51 -0400

    Add steps and dropout flags

--- scripts/tiny.sh
+++ scripts/tiny.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+## Regarding LoRA parameters, we set r = 64, α = 16, and LoRA dropout of 0.1 for models up to 13B and 0.05 for 33B and 65B models.
+## We employ the paged AdamW optimizer with a beta2 value of 0.999, and a learning rate of 2e-4 for models up to 13B and 1e-4 for 33B and 65B models,
+## limiting the maximum gradient norm to 0.3 and adopting a constant learning rate strategy. Fine-tuning was executed for 10,000 and 20,000 steps on
+## the Alpaca and FLAN v2 datasets, respectively, utilizing a batch size 16.
 
 gpu=""
 lora_rank="64"
 learning_rate=""
 layers=""
 extra_name=""
-annotation=""
 wandb=""
 quant_ema_decay=0.99
 eval_step_zero=0
@@ -13,13 +16,16 @@ hr_lora_r=""
 model="huggyllama/llama-7b"
 dataset="oasst1"
 steps=0
+eval_steps=187
+save_steps=500
+dropout=0.1
 
 qlora="[FILL IN]"
 
-usage() { echo "Usage: $0 -g <gpu_no> -h <hr lora rank csv> -m <model> -d <dataset> -s <steps>[-w <use wanddb> -l <learning rate (og: 0.0002)> -q <quant ema decay> -e <extra name> -a <annotation> -t <eval step zero>" 1>&2; exit 1; }
+usage() { echo "Usage: $0 -g <gpu_no> -h <hr lora rank csv> -m <model> -d <dataset> -s <steps>[-w <use wanddb> -v <eval_steps> -a <save_steps> -r <dropout> -l <learning rate (og: 0.0002)> -q <quant ema decay> -e <extra name> -t <eval step zero>" 1>&2; exit 1; }
 
 ## if w flag is present, set wandb to true
-while getopts ":g:h:l:e:a:w:d:t:m:d:q:s:" o; do
+while getopts ":g:h:l:e:a:w:d:t:m:d:q:s:v:a:r" o; do
     case "${o}" in
         g)
             gpu=${OPTARG}
@@ -34,7 +40,7 @@ while getopts ":g:h:l:e:a:w:d:t:m:d:q:s:" o; do
             extra_name=${OPTARG}
             ;;
         a)
-            annotation=${OPTARG}
+            save_steps=${OPTARG}
             ;;
         w)
             wandb="--report_to wandb"
@@ -54,6 +60,12 @@ while getopts ":g:h:l:e:a:w:d:t:m:d:q:s:" o; do
         s)
             steps=${OPTARG}
             ;;
+        v)
+            eval_steps=${OPTARG}
+            ;;
+        r)
+            dropout=${OPTARG}
+            ;;
         *)
             usage
             ;;
@@ -63,7 +75,6 @@ shift $((OPTIND-1))
 
 echo "lora_rank = ${lora_rank}"
 echo "learning_rate = ${learning_rate}"
-echo "annotation = ${annotation}"
 echo "Recording to wandb? = ${wandb}"
 echo "quant_ema_decay = ${quant_ema_decay}"
 echo "eval_step_zero = ${eval_step_zero}"
@@ -92,8 +103,8 @@ CUDA_VISIBLE_DEVICES=$gpu python $qlora \
     --logging_steps 1 \
     --save_strategy steps \
     --data_seed 42 \
-    --save_steps 500 \
-    --save_total_limit 40 \
+    --save_steps $save_steps \
+    --save_total_limit 50 \
     --evaluation_strategy steps \
     --eval_dataset_size 1024 \
     --max_eval_samples 1000 \
@@ -122,11 +133,11 @@ CUDA_VISIBLE_DEVICES=$gpu python $qlora \
     --per_device_train_batch_size 1 \
     --gradient_accumulation_steps 16 \
     --max_steps $steps \
-    --eval_steps 187 \
+    --eval_steps $eval_steps \
     --learning_rate $learning_rate \
     --adam_beta2 0.999 \
     --max_grad_norm 0.3 \
-    --lora_dropout 0.05 \
+    --lora_dropout $dropout \
     --weight_decay 0.0 \
     --quant_ema_decay $quant_ema_decay \
     --eval_step_zero $eval_step_zero \
@@ -135,5 +146,4 @@ CUDA_VISIBLE_DEVICES=$gpu python $qlora \
     $wandb \
     --run_name $name \
     --gpu $gpu \
-    --annotation $annotation \
     --use_auth_token True
